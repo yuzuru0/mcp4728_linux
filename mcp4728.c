f7b51b0e26b6cda38c138e739dcaeff1ed96571c
Yuzuru Ohba <ohba@sendai-nct.ac.jp>
2024-05-28 13:16:32 +0900

    コンフィグレジスタの値定義

--- mcp4728.c
+++ mcp4728.c
@@ -4,6 +4,22 @@
 
 int main(void)
 {
+	unsigned char dev_addr = 0x60;
+	unsigned char reg_addr = 0xC0;
+
+	mcp4728_config_data mcp4728_config;
+
+	mcp4728_config.COMMAND = SINGLE_WRITE;
+	mcp4728_config.DAC_CH = CH_A;
+	mcp4728_config.UDAC = EN_LATCH;
+	mcp4728_config.VREF = VREF_INTERNAL;
+	mcp4728_config.PD = PD_NORMAL;
+	mcp4728_config.Gx = GAIN_X2;
+
+	mcp4728_config.DAC_DATA = 0x0FFF;
+
+
+
 	
 	return 0;
 }
--- mcp4728.h
+++ mcp4728.h
@@ -1,6 +1,28 @@
 #ifndef __MCP4728_H__
 #define __MCP4728_H__
 
+#define SINGLE_WRITE 0x0B
+
+#define CH_A 0x00
+#define CH_B 0x01
+#define CH_C 0x02
+#define CH_D 0x03
+
+#define EN_LATCH 0x00
+
+#define VREF_INTERNAL 0x01
+#define VREF_EXTERNAL 0x00
+
+#define PD_NORMAL 0x00
+#define PD_1K	0x01
+#define PD_100K 0x02
+#define PD_500K 0x03
+
+#define GAIN_X2 0x01
+#define GAIN_X1 0x00
+
+
+
 typedef union
 {
 	unsigned int i2c_data;
