4cbce113e647d19cfb64b0425d35e4cda34d1096
Yuzuru Ohba <ohba@sendai-nct.ac.jp>
2024-05-28 13:45:40 +0900

    テストコード

--- mcp4728.c
+++ mcp4728.c
@@ -7,18 +7,20 @@ int main(void)
 	unsigned char dev_addr = 0x60;
 	unsigned char reg_addr = 0xC0;
 
-	mcp4728_config_data mcp4728_config;
+	mcp4728_config_data mcp4728;
 
-	mcp4728_config.COMMAND = SINGLE_WRITE;
-	mcp4728_config.DAC_CH = CH_A;
-	mcp4728_config.UDAC = EN_LATCH;
-	mcp4728_config.VREF = VREF_INTERNAL;
-	mcp4728_config.PD = PD_NORMAL;
-	mcp4728_config.Gx = GAIN_X2;
+	mcp4728.config.COMMAND = SINGLE_WRITE;
+	mcp4728.config.DAC_CH = CH_A;
+	mcp4728.config.UDAC = EN_LATCH;
+	mcp4728.config.VREF = VREF_INTERNAL;
+	mcp4728.config.PD = PD_NORMAL;
+	mcp4728.config.Gx = GAIN_X2;
 
-	mcp4728_config.DAC_DATA = 0x0FFF;
+	mcp4728.config.DAC_DATA = 0x0FFF;
 
+	printf("%x - %x %x %x %d\n",mcp4728.i2c_data, mcp4728.byte[0],mcp4728.byte[1],mcp4728.byte[2], sizeof(mcp4728.byte));
 
+	i2c_write(dev_addr, reg_addr, mcp4728.byte, sizeof(mcp4728.byte)); 
 
 	
 	return 0;
