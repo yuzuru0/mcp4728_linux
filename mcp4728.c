9c4b57049f313cbd9f53ed06780c793185f4999b
yuzuru ohba <ohba@sendai-nct.ac.jp>
2024-05-31 18:24:47 +0900

    アドレス計算ミス修正

--- mcp4728.c
+++ mcp4728.c
@@ -15,13 +15,21 @@ int output_da(int ch, float voltage)
 		return -1;
 	}
 
-	if(voltage < DAC_MIN_VOLT || voltage> DAC_MAX_VOLT)
+	if(voltage < DAC_MIN_VOLT)
 	{
 		ret = -2;
+		voltage = DAC_MIN_VOLT;
 		printf("DACの出力範囲を超えています\n");
 	}
 
-	reg_addr = DAC_ADDRESS_BASE || ch<<1;
+	if(voltage > DAC_MAX_VOLT)
+	{
+		ret = -3;
+		voltage = DAC_MAX_VOLT;
+		printf("DACの出力範囲を超えています\n");
+	}
+
+	reg_addr = DAC_ADDRESS_BASE | ch<<1;
 
 	mcp4728.config.VREF = VREF_INTERNAL;
 	mcp4728.config.PD = PD_NORMAL;
@@ -30,7 +38,7 @@ int output_da(int ch, float voltage)
 	mcp4728.config.DAC_DATA = (int)(voltage * DAC_MAX_DATA / DAC_MAX_VOLT);
 
 
-	printf("%x - %x %x %x %d\n",mcp4728.i2c_data, mcp4728.byte[0],mcp4728.byte[1], sizeof(mcp4728.byte));
+	printf("%x %x \n",reg_addr, mcp4728.i2c_data );
 
 
 //	i2c_write(dev_addr, reg_addr, mcp4728.byte, sizeof(mcp4728.byte)); 
@@ -41,6 +49,6 @@ int output_da(int ch, float voltage)
 int main(void)
 {
 
-	output_da(CH_A, 4.096);
+	output_da(CH_A, 0.0);
 	return 0;
 }
