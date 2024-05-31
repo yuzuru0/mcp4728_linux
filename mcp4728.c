71467f6b96dc8923b9b413b76354d04ea5b38821
yuzuru ohba <ohba@sendai-nct.ac.jp>
2024-05-31 18:09:06 +0900

    デバッグ用コード更新

--- mcp4728.c
+++ mcp4728.c
@@ -27,28 +27,15 @@ int output_da(int ch, float voltage)
 	printf("%x - %x %x %x %d\n",mcp4728.i2c_data, mcp4728.byte[0],mcp4728.byte[1],mcp4728.byte[2], sizeof(mcp4728.byte));
 
 
-	i2c_write(dev_addr, reg_addr, mcp4728.byte, sizeof(mcp4728.byte)); 
+//	i2c_write(dev_addr, reg_addr, mcp4728.byte, sizeof(mcp4728.byte)); 
 
 	return ret;
 
 }
 int main(void)
 {
-	unsigned char dev_addr = 0x60;
-	unsigned char reg_addr = DAC_ADDRESS_A;
 
-	mcp4728_config_data mcp4728;
+	output_da(CH_A, 0.0);
 
-	mcp4728.config.VREF = VREF_INTERNAL;
-	mcp4728.config.PD = PD_NORMAL;
-	mcp4728.config.Gx = GAIN_X2;
-
-	mcp4728.config.DAC_DATA = 0x07FF;
-
-	printf("%x - %x %x %x %d\n",mcp4728.i2c_data, mcp4728.byte[0],mcp4728.byte[1],mcp4728.byte[2], sizeof(mcp4728.byte));
-
-	i2c_write(dev_addr, reg_addr, mcp4728.byte, sizeof(mcp4728.byte)); 
-
-	
 	return 0;
 }
