1ffda5d6706acffab0dfd5ea9354db0fb3319dd2
Yuzuru Ohba <ohba@sendai-nct.ac.jp>
2024-06-03 09:57:37 +0900

    テスト用コード

--- mcp4728.c
+++ mcp4728.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "mcp4728.h"
 #include "i2c_interface.h"
 
@@ -44,9 +45,16 @@ int output_da(int ch, float voltage)
 	return ret;
 
 }
-int main(void)
+int main(int argc, char *argv[])
 {
+	float voltage;
+	int ch;
+	char *endptr;
 
-	output_da(CH_A, 2.0);
+	ch = strtol(argv[1], &endptr,10);
+	voltage =strtof(argv[2],&endptr);
+
+
+	output_da(ch, voltage);
 	return 0;
 }
